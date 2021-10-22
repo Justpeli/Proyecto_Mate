12b244a2fdec3a08f6e8f30eeaf48eeb6b7a0a91
JesusRuiz02 <zap295@amerike.edu.mx>
2021-10-22 08:47:58 -0500

    aaaa

---
+++ My project/Assets/Scripts/RandomTexture.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class RandomTexture : MonoBehaviour
+{
+
+    
+
+    private Spawn _spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void random()
+    {
+        
+        
+    }
+}
--- My project/Assets/Scripts/Spawn.cs
+++ My project/Assets/Scripts/Spawn.cs
@@ -6,9 +6,17 @@ public class Spawn : MonoBehaviour
 {
     public int numberToSpawn;
     public List<GameObject> spawnPool;
+    public List<Material> a;
     public GameObject quad;
     public GameObject planet;
-
+    public int random_index;
+    private int randomname;
+    public int x;
+    public Material Meme;
+    public Material black;
+    public Material blue;
+    public Material red;
+    public Material yellow;
     void Start()
     {
         spawnObjects();
@@ -24,6 +32,7 @@ public class Spawn : MonoBehaviour
 
         float screenX, screenY, screenZ;
         Vector3 pos;
+        Material random;
 
         for (int i = 0; i < numberToSpawn; i++)
         {
@@ -34,8 +43,31 @@ public class Spawn : MonoBehaviour
             screenY = Random.Range(c.bounds.min.y, c.bounds.max.y);
             screenZ = Random.Range(c.bounds.min.z, c.bounds.max.z);
             pos = new Vector3(screenX, screenY, screenZ);
-
             Instantiate(toSpawn, pos, toSpawn.transform.rotation);
+            
+            
+            /* x = Random.Range(1, 5);
+             switch (x)
+             {
+                 case 1:
+                     toSpawn.GetComponent<Renderer>().material = yellow;
+                     break;
+                 case 2:
+                     toSpawn.GetComponent<Renderer>().material = Meme;
+                     break;
+              
+                 case 3 :
+                     toSpawn.GetComponent<Renderer>().material = red;
+                     break;
+              
+                 case 4:
+                     toSpawn.GetComponent<Renderer>().material = blue;
+                     break;
+                 case 5:
+                     toSpawn.GetComponent<Renderer>().material = black;
+                     break;
+             }
+ */
 
         }
     }
@@ -47,4 +79,17 @@ public class Spawn : MonoBehaviour
         }
     }
 
+    /*void randomtexture()
+    {
+        GameObject mesh;
+        for (int i = 0; i < numberToSpawn; i++)
+        {
+            random_index = Random.Range(0, spawnPool.Count);
+            mesh = spawnPool[random_index];
+           
+
+
+        }
+    }
+*/
 }
